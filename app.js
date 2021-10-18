f14f52f032f799d91b0c18f7a0e408fc1f752922
dmitry_papka <d.papka@whisbi.com>
2021-10-18 20:58:19 -0300

    test

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/set-cookie', (req, res) => {
+    res.cookie('testCookie', 'testValue', {
+        domain: 'localhost'
+    });
+
+    res.status(200).json({ ok: true });
+});
+
+app.get('/get-cookie', (req, res) => {
+    const cookies = req.cookies;
+    console.log(cookies);
+    res.status(200).json(cookies);
+});
+
+app.listen(80, () => {
+    console.log('server started');
+});
